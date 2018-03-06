fb1db97fdb8aefdd1a17d6796210ea2143e5163b
ybin <zyb0807@gmail.com>
2018-03-06 15:42:26 +0800

    Init

---
+++ lib/say.c
@@ -0,0 +1,7 @@
+#include <config.h>
+#include <stdio.h>
+
+void say_hello(void)
+{
+    puts("Hello Libtool.");
+}
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "say.h"
+
+int main(void)
+{
+    say_hello();
+    return 0;
+}
